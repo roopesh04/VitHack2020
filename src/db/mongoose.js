70918a00dc80cb69b5840e5f6b7277e363a05149
roopesh04 <53055302+roopesh04@users.noreply.github.com>
2021-01-29 00:35:43 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 require('dotenv').config()
 
 url=process.env.URL
-mongoose.connect('mongodb+srv://testapp:roopesh123@cluster0.2zqrx.mongodb.net/vithack2020?retryWrites=true&w=majority',{
+mongoose.connect(url,{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology:true
